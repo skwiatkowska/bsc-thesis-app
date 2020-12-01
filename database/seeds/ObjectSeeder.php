e8fc590f21c9e51d73e4fd7a82b24484734861f4
Sylwia <sylwia664@vp.pl>
2020-12-01 16:50:57 +0100

    db seeder part 3

--- database/seeds/ObjectSeeder.php
+++ database/seeds/ObjectSeeder.php
@@ -21,49 +21,57 @@ class ObjectSeeder extends Seeder {
         $cypher = "MATCH (n) DETACH DELETE n;";
         DB::select($cypher);
 
-        $users = factory(User::class)->times(15)->create();
-        $books = factory(Book::class)->times(10)->create();
-        $authors = factory(Author::class)->times(10)->create();
+        $users = factory(User::class)->times(10)->create();
+        $books = factory(Book::class)->times(8)->create();
+        $authors = factory(Author::class)->times(7)->create();
         $publishers = factory(Publisher::class)->times(5)->create();
         $categories = factory(Category::class)->times(5)->create();
 
-        $action = ["reservation", "borrowing", "return", "reservation", "borrowing", "return", null];
+        $action = ["reservation", "borrowing", "return", null];
+
 
         foreach ($books as $book) {
             $bookItems = factory(BookItem::class)->times(rand(1, 2))->create();
-            foreach ($bookItems as $bookItem) {
-                $book->bookItems()->save($bookItem);
-                $randomAction = array_rand($action);
+            for ($i = 0; $i < 2; $i++) {
+                foreach ($bookItems as $bookItem) {
+                    foreach ($book->bookItems as $b) {
+                        if ($b->id == $bookItem->id) {
+                            continue;
+                        }
+                    }
+                    $book->bookItems()->save($bookItem);
+                    $randomAction = array_rand($action);
 
-                if ($randomAction == 0 || $randomAction == 3) { //reservation
-                    $user = $users[rand(0, $users->count() - 1)];
-                    $reservation =  new Reservation(['due_date' =>  new DateTime("+3 days")]);
-                    $user->reservations($bookItem)->save($reservation);
-                } elseif ($randomAction == 1 || $randomAction == 4) { //borrowing
-                    $borrowDate = [null, "-1 day", "-2 days", "-3 days"];
-                    $randomBorrowDate = $borrowDate[array_rand($borrowDate)];
-                    $dueDate = ["+1 month", "+14 days", "+3 days"];
-                    $randomDueDate = $dueDate[array_rand($dueDate)];
-                    $user = $users[rand(0, $users->count() - 1)];
-                    $borrowing =  new Borrowing(['borrow_date' => new DateTime($randomBorrowDate), 'due_date' => new DateTime($randomDueDate), 'was_prolonged' => false]);
-                    $user->borrowings($bookItem)->save($borrowing);
-                } elseif ($randomAction == 2 || $randomAction == 5) { //return
-                    $borrowDate = [null, "-1 month", "-2 months", "-14 days"];
-                    $randomBorrowDate = $borrowDate[array_rand($borrowDate)];
-                    $dueDate = ["+1 day", "-10 days", "-5 days", "+2 days"];
-                    $randomDueDate = $dueDate[array_rand($dueDate)];
-                    $user = $users[rand(0, $users->count() - 1)];
-                    $due = new DateTime($randomDueDate);
-                    $borrowing =  new Borrowing(['borrow_date' => new DateTime($randomBorrowDate), 'due_date' => $due, 'was_prolonged' => false]);
-                    $user->borrowings($bookItem)->save($borrowing);
+                    if ($randomAction == 0) { //reservation
+                        $user = $users[rand(0, $users->count() - 1)];
+                        $reservation =  new Reservation(['due_date' =>  new DateTime("+3 days")]);
+                        $user->reservations($bookItem)->save($reservation);
+                    } elseif ($randomAction == 1) { //borrowing
+                        $borrowDate = [null, "-1 day", "-2 days", "-3 days"];
+                        $randomBorrowDate = $borrowDate[array_rand($borrowDate)];
+                        $dueDate = ["+1 month", "+14 days", "+3 days"];
+                        $randomDueDate = $dueDate[array_rand($dueDate)];
+                        $user = $users[rand(0, $users->count() - 1)];
+                        $borrowing =  new Borrowing(['borrow_date' => new DateTime($randomBorrowDate), 'due_date' => new DateTime($randomDueDate), 'was_prolonged' => false]);
+                        $user->borrowings($bookItem)->save($borrowing);
+                    } elseif ($randomAction == 2) { //return
+                        $borrowDate = [null, "-1 month", "-2 months", "-14 days"];
+                        $randomBorrowDate = $borrowDate[array_rand($borrowDate)];
+                        $dueDate = ["+1 day", "-10 days", "-5 days", "+2 days"];
+                        $randomDueDate = $dueDate[array_rand($dueDate)];
+                        $user = $users[rand(0, $users->count() - 1)];
+                        $due = new DateTime($randomDueDate);
+                        $borrowing =  new Borrowing(['borrow_date' => new DateTime($randomBorrowDate), 'due_date' => $due, 'was_prolonged' => false]);
+                        $user->borrowings($bookItem)->save($borrowing);
 
-                    $borrowing->update(['actual_return_date' => new DateTime()]);
-                    $bookItem->update(['status' => BookItem::AVAILABLE]);
-                    if ($borrowing->due_date < new DateTime()) {
-                        $now = new DateTime();
-                        $interval = $now->diff($due);
-                        $fee = $interval->d * 0.5;
-                        $borrowing->overdue_fee = $fee;
+                        $borrowing->update(['actual_return_date' => new DateTime()]);
+                        $bookItem->update(['status' => BookItem::AVAILABLE]);
+                        if ($borrowing->due_date < new DateTime()) {
+                            $now = new DateTime();
+                            $interval = $now->diff($due);
+                            $fee = $interval->d * 0.5;
+                            $borrowing->overdue_fee = $fee;
+                        }
                     }
                 }
             }
